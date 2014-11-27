ded1906a567072502d331a5283d0074954627441
Kandarp <kandarp.9@gmail.com>
2014-11-27 03:19:32 -0800

    Bug fixes

--- src/mygame/GameStarter.java
+++ src/mygame/GameStarter.java
@@ -17,6 +17,7 @@ import com.jme3.bullet.collision.shapes.MeshCollisionShape;
 import com.jme3.bullet.control.CharacterControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.control.VehicleControl;
+import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
@@ -41,43 +42,66 @@ import com.jme3.util.TangentBinormalGenerator;
 import com.jme3.water.WaterFilter;
 import observer.CarControlActionListener;
 
-
 public class GameStarter extends SimpleApplication {
 
     // kandarp
     private static GameStarter instance = new GameStarter();
-    
-    public static GameStarter getInstance(){
+
+    public static GameStarter getInstance() {
         return instance;
     }
-    private GameStarter(){}
-    
+
+    private GameStarter() {
+    }
     BulletAppState bulletAppState;
     VehicleControl vehicle;
     //CharacterControl vehicle;
-    
     final float accelerationForce = 1000.0f;
     final float brakeForce = 100.0f;
     float steeringValue = 0;
     float accelerationValue = 0;
     Vector3f jumpForce = new Vector3f(0, 3000, 0);
-    
     // kandarp
-    
-    
     StrategyColorInterface strategy;
-    
+
     @Override
     public void simpleInitApp() {
-        
+
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
-        
-        createPhysicsTestWorld();
-                
-//        initTerrain();
-//        initPPcWater();
-//        
+
+        initTerrain();
+        initPPcWater();
+
+        // kandarp
+        setupKeys();
+        buildPlayer();
+        Vector3f v = vehicle.getPhysicsLocation();
+
+        strategy = new ConcreteBlue();
+
+        v.z += 10;
+        initGumballs(v);
+
+        v.z += 20;
+        v.x += 10;
+        initGumballs(v);
+
+        strategy = new ConcretePink();
+        v.z += 10;
+        v.x += -20;
+        initGumballs(v);
+
+
+        strategy = new ConcreteRed();
+        v.z += 10;
+        v.x += -20;
+        initGumballs(v);
+
+
+        initWall(v);
+
+
 //        flyCam.setMoveSpeed(50);
 //        initLight(10f, 10f, 10f);
 //        initAnimation(10f, 10f, 10f);
@@ -130,164 +154,103 @@ public class GameStarter extends SimpleApplication {
 //        initGumballs(-450.0f , 3.0f , 160.0f);
 //        strategy=new ConcreteRed();
 //        initGumballs(450.0f , 3.0f , -40.0f);
-//        
-        // kandarp
-          setupKeys();
-        buildPlayer();
+
+
+
+
+
+        //flyCam.setEnabled(true);
+        ChaseCamera chaseCam = new ChaseCamera(cam, car, inputManager);
+        chaseCam.setSmoothMotion(true);
         //kandarp
-        
+
     }
 
-//    @Override
-//    public void simpleUpdate(float tpf) {
-//        //TODO: add update code
-//    }
-
-//    @Override
-//    public void simpleRender(RenderManager rm) {
-//        //TODO: add render code
-//    }
-    
-     void initTerrain(){
-          
-         
-        AbstractHeightMap heightmap = null;
-        Texture heightMapImage = assetManager.loadTexture("Textures/heightmap1.jpg");
-        heightmap = new ImageBasedHeightMap(heightMapImage.getImage());
-        //heightmap.load();
-        
-         ITerrain customTerrain = 
-                new RoadTerrain(new GrassTerrain(new BasicTerrain()));
-        
-        Material mat_terrain = new Material(assetManager, "Common/MatDefs/Terrain/Terrain.j3md");
-        mat_terrain = customTerrain.addTexture(mat_terrain, this);
-        
-        TerrainQuad terrain = new TerrainQuad("my terrain", 65, 513, heightmap.getHeightMap());
-        terrain.setMaterial(mat_terrain);
-        terrain.setLocalTranslation(0, 1, 0);
-        terrain.setLocalScale(2f, 1f, 2f);
-        rootNode.attachChild(terrain);
-        
-        //TerrainLodControl control = new TerrainLodControl(terrain, getCamera());
-        //terrain.addControl(control);
- 
-        terrain.addControl(new RigidBodyControl());
-        
-    
-        getPhysicsSpace().add(terrain);
-        
-        //kandarp
-       // TerrainLodControl control = new TerrainLodControl(terrain, getCamera());
-        //terrain.addControl(control);
-        
-        
-     }
-    
-     public void initPPcWater(){
-        Vector3f lightDir = new Vector3f(1, -1, 5);
-        WaterFilter water = new WaterFilter(rootNode, lightDir);
-        water.setCenter(Vector3f.ZERO);
-        water.setRadius(2600);
-        
-        water.setWaveScale(0.001f);
-        water.setMaxAmplitude(2f);
-        water.setFoamExistence(new Vector3f(1f, 3, 0.5f));
-        water.setFoamTexture((Texture2D) assetManager.loadTexture("Textures/texture_Foam.jpg"));
-        water.setRefractionStrength(0.5f);
-        water.setWaterHeight(-50.0f);
-        
-        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
-        fpp.addFilter(water);
-        viewPort.addProcessor(fpp);
-         
-    } 
-     
     public void initAnimation(float x, float y, float z) {
-      Node player = (Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
-      player.setLocalScale(3f);
-      player.setLocalTranslation(x,y,z);
-      player.addControl( new RigidBodyControl(0));
-              
-      rootNode.attachChild(player);
-      getPhysicsSpace().add(player);
+        Node player = (Node) assetManager.loadModel("Models/Oto/Oto.mesh.xml");
+        player.setLocalScale(3f);
+        player.setLocalTranslation(x, y, z);
+        player.addControl(new RigidBodyControl(0));
+
+        rootNode.attachChild(player);
+        getPhysicsSpace().add(player);
     }
-    
+
     public void initLight(float x, float y, float z) {
         DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(x,y,z));
+        sun.setDirection(new Vector3f(x, y, z));
         rootNode.addLight(sun);
 
     }
-    
-    public void initWall(float x, float y, float z) {
-    Box box = new Box(20f, 20f, 20f);
-    Spatial wall = new Geometry("Box", box);
-    //box.setTextureMode(Box.TextureMode.Projected); // better quality on spheres
-    TangentBinormalGenerator.generate(box);           // for lighting effect
-    Material boxMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-    boxMat.setTexture("ColorMap", assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
-    wall.setMaterial(boxMat);
-    wall.setLocalTranslation(x, y, z); // Move it a bit
-    //boxGeo.rotate(1.6f, 0, 0);          // Rotate it a bit
-    wall.addControl(new RigidBodyControl(0));
-    rootNode.attachChild(wall);
-    getPhysicsSpace().add(wall);
+
+    public void initWall(Vector3f v) {
+        Box box = new Box(3f, 3f, 3f);
+        Spatial wall = new Geometry("Box", box);
+        //box.setTextureMode(Box.TextureMode.Projected); // better quality on spheres
+        TangentBinormalGenerator.generate(box);           // for lighting effect
+        Material boxMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        boxMat.setTexture("ColorMap", assetManager.loadTexture("Textures/Terrain/BrickWall/BrickWall.jpg"));
+        wall.setMaterial(boxMat);
+        wall.setLocalTranslation(v); // Move it a bit
+        //boxGeo.rotate(1.6f, 0, 0);          // Rotate it a bit
+        wall.addControl(new RigidBodyControl(0));
+        rootNode.attachChild(wall);
+        getPhysicsSpace().add(wall);
     }
-    
+
     public void initSphere(float x, float y, float z) {
-        Sphere sphereMesh = new Sphere(32,32, 20f);
+        Sphere sphereMesh = new Sphere(32, 32, 20f);
         Geometry sphereGeo = new Geometry("Shiny rock", sphereMesh);
         sphereMesh.setTextureMode(Sphere.TextureMode.Projected); // better quality on spheres
         TangentBinormalGenerator.generate(sphereMesh);           // for lighting effect
-        Material sphereMat = new Material(assetManager, 
-        "Common/MatDefs/Light/Lighting.j3md");
-        sphereMat.setTexture("DiffuseMap", 
-        assetManager.loadTexture("Textures/Terrain/Rocky/RockyNormals.jpg"));
-        sphereMat.setTexture("NormalMap", 
-        assetManager.loadTexture("Textures/Terrain/Pond/Pond.jpg"));
-        sphereMat.setBoolean("UseMaterialColors",true);    
-        sphereMat.setColor("Diffuse",ColorRGBA.Red);
-        sphereMat.setColor("Specular",ColorRGBA.Yellow);
+        Material sphereMat = new Material(assetManager,
+                "Common/MatDefs/Light/Lighting.j3md");
+        sphereMat.setTexture("DiffuseMap",
+                assetManager.loadTexture("Textures/Terrain/Rocky/RockyNormals.jpg"));
+        sphereMat.setTexture("NormalMap",
+                assetManager.loadTexture("Textures/Terrain/Pond/Pond.jpg"));
+        sphereMat.setBoolean("UseMaterialColors", true);
+        sphereMat.setColor("Diffuse", ColorRGBA.Red);
+        sphereMat.setColor("Specular", ColorRGBA.Yellow);
         sphereMat.setFloat("Shininess", 64f);  // [0,128]
         sphereGeo.setMaterial(sphereMat);
-        sphereGeo.setLocalTranslation(x,y,z); // Move it a bit
+        sphereGeo.setLocalTranslation(x, y, z); // Move it a bit
         sphereGeo.rotate(1.6f, 0, 0);          // Rotate it a bit
         sphereGeo.addControl(new RigidBodyControl(0));
         rootNode.attachChild(sphereGeo);
         getPhysicsSpace().add(sphereGeo);
     }
 
-public void initGumballs(float x, float y, float z)
-     {
-           
-        Vector3f v = new Vector3f(x,y,z);
+    public void initGumballs(Vector3f v) {
+
+        //Vector3f v = new Vector3f(x, y, z);
         Sphere s = new Sphere(8, 8, 1, false, true);
         Geometry geoms = new Geometry("Sphere", s);
         Material mats1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mats1.setColor("Color", strategy.getColor());
         geoms.setMaterial(mats1);
         geoms.setLocalTranslation(v);
-        
+
         geoms.addControl(new RigidBodyControl(0));
         rootNode.attachChild(geoms);
         getPhysicsSpace().add(geoms);
 
-         
-     }
+
+    }
 
     //kandarp
- private void setupKeys() {
-        
-        inputManager.addMapping("Lefts", new KeyTrigger(KeyInput.KEY_H));
-        inputManager.addMapping("Rights", new KeyTrigger(KeyInput.KEY_K));
-        inputManager.addMapping("Ups", new KeyTrigger(KeyInput.KEY_U));
-        inputManager.addMapping("Downs", new KeyTrigger(KeyInput.KEY_J));
-        inputManager.addMapping("N", new KeyTrigger(KeyInput.KEY_N));
+    private void setupKeys() {
+
+        inputManager.addMapping("Lefts", new KeyTrigger(KeyInput.KEY_LEFT));
+        inputManager.addMapping("Rights", new KeyTrigger(KeyInput.KEY_RIGHT));
+        inputManager.addMapping("Ups", new KeyTrigger(KeyInput.KEY_UP));
+        inputManager.addMapping("Downs", new KeyTrigger(KeyInput.KEY_DOWN));
+        inputManager.addMapping("B", new KeyTrigger(KeyInput.KEY_B));
         inputManager.addMapping("Space", new KeyTrigger(KeyInput.KEY_SPACE));
         inputManager.addMapping("Reset", new KeyTrigger(KeyInput.KEY_RETURN));
-        
+
         CarControlActionListener actionListener = new CarControlActionListener();
-        
+
         inputManager.addListener(actionListener, "Lefts");
         inputManager.addListener(actionListener, "Rights");
         inputManager.addListener(actionListener, "Ups");
@@ -295,29 +258,31 @@ public void initGumballs(float x, float y, float z)
         inputManager.addListener(actionListener, "Space");
         inputManager.addListener(actionListener, "Reset");
     }
+    Spatial car = null;
 
     private void buildPlayer() {
 
-        Spatial car = assetManager.loadModel("Models/Ferrari/Car.j3o");
+        car = assetManager.loadModel("Models/Ferrari/Car.j3o");
+
         vehicle = car.getControl(VehicleControl.class);
         vehicle.setPhysicsLocation(new Vector3f(-10, 10, 10));
-        vehicle.setGravity(new Vector3f());
-        
+        vehicle.setGravity(new Vector3f(30, 30, 30));
+
         car.addControl(vehicle);
-        
+
         rootNode.attachChild(car);
         bulletAppState.getPhysicsSpace().add(car);
     }
-    
-    private PhysicsSpace getPhysicsSpace(){
+
+    private PhysicsSpace getPhysicsSpace() {
         return bulletAppState.getPhysicsSpace();
     }
-    
+
     @Override
     public void simpleUpdate(float tpf) {
         cam.lookAt(vehicle.getPhysicsLocation(), Vector3f.UNIT_Y);
     }
-    
+
     public BulletAppState getBulletAppState() {
         return bulletAppState;
     }
@@ -333,6 +298,7 @@ public void initGumballs(float x, float y, float z)
     public void setVehicle(VehicleControl vehicle) {
         this.vehicle = vehicle;
     }
+
     public float getSteeringValue() {
         return steeringValue;
     }
@@ -356,67 +322,72 @@ public void initGumballs(float x, float y, float z)
     public void setJumpForce(Vector3f jumpForce) {
         this.jumpForce = jumpForce;
     }
-    
-     public float getAccelerationForce() {
+
+    public float getAccelerationForce() {
         return accelerationForce;
     }
 
     public float getBrakeForce() {
         return brakeForce;
     }
- 
-    
-     public void createPhysicsTestWorld() {
+
+    public void initTerrain() {
+
+        TerrainQuad terrain;
+        Material mat_terrain;
         
         DirectionalLight light = new DirectionalLight();
-    light.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
-    light.setColor(ColorRGBA.White);
-    
-//        AmbientLight light = new AmbientLight();
-//        light.setColor(ColorRGBA.LightGray);
+        light.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        light.setColor(ColorRGBA.White);
         rootNode.addLight(light);
 
-        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        material.setTexture("ColorMap", assetManager.loadTexture("Interface/Logo/Monkey.jpg"));
-
-        Box floorBox = new Box(140, 0.25f, 140);
-        Geometry floorGeometry = new Geometry("Floor", floorBox);
-        floorGeometry.setMaterial(material);
-        floorGeometry.setLocalTranslation(0, -5, 0);
-//        Plane plane = new Plane();
-//        plane.setOriginNormal(new Vector3f(0, 0.25f, 0), Vector3f.UNIT_Y);
-//        floorGeometry.addControl(new RigidBodyControl(new PlaneCollisionShape(plane), 0));
-        floorGeometry.addControl(new RigidBodyControl(0));
-        rootNode.attachChild(floorGeometry);
-        getPhysicsSpace().add(floorGeometry);
-
-        //movable boxes
-        for (int i = 0; i < 12; i++) {
-            Box box = new Box(0.25f, 0.25f, 0.25f);
-            Geometry boxGeometry = new Geometry("Box", box);
-            boxGeometry.setMaterial(material);
-            boxGeometry.setLocalTranslation(i, 5, -3);
-            //RigidBodyControl automatically uses box collision shapes when attached to single geometry with box mesh
-            boxGeometry.addControl(new RigidBodyControl(2));
-            rootNode.attachChild(boxGeometry);
-            getPhysicsSpace().add(boxGeometry);
-        }
-
-        //immovable sphere with mesh collision shape
-        Sphere sphere = new Sphere(8, 8, 1);
-        Geometry sphereGeometry = new Geometry("Sphere", sphere);
-        sphereGeometry.setMaterial(material);
-        sphereGeometry.setLocalTranslation(4, -4, 2);
-        sphereGeometry.addControl(new RigidBodyControl(new MeshCollisionShape(sphere), 0));
-        rootNode.attachChild(sphereGeometry);
-        getPhysicsSpace().add(sphereGeometry);
+        ITerrain customTerrain = 
+                new RoadTerrain(new GrassTerrain(new BasicTerrain()));
         
-        	strategy = new ConcreteBlue();
-        initGumballs(10 , -10 , 3);
         
+        
+        mat_terrain = new Material(assetManager,
+                "Common/MatDefs/Terrain/Terrain.j3md");
+        
+        mat_terrain = customTerrain.addTexture(mat_terrain, this);
+
+        AbstractHeightMap heightmap = null;
+        Texture heightMapImage = assetManager.loadTexture(
+                "Textures/heightmapK.png");
+        heightmap = new ImageBasedHeightMap(heightMapImage.getImage());
+        heightmap.load();
+        
+        terrain = new TerrainQuad("my terrain", 65, 513, null);
+
+        terrain.setMaterial(mat_terrain);
+        terrain.setLocalTranslation(0, -1, 0);
+        terrain.setLocalScale(2f, 1f, 2f);
+        rootNode.attachChild(terrain);
+
+
+        terrain.addControl(new RigidBodyControl(0));
+
+        getPhysicsSpace().add(terrain);
 
     }
-    
+
+    public void initPPcWater() {
+        Vector3f lightDir = new Vector3f(1, -1, 5);
+        WaterFilter water = new WaterFilter(rootNode, lightDir);
+        water.setCenter(Vector3f.ZERO);
+        water.setRadius(2600);
+
+        water.setWaveScale(0.001f);
+        water.setMaxAmplitude(2f);
+        water.setFoamExistence(new Vector3f(1f, 3, 0.5f));
+        water.setFoamTexture((Texture2D) assetManager.loadTexture("Textures/texture_Foam.jpg"));
+        water.setRefractionStrength(0.5f);
+        water.setWaterHeight(-50.0f);
+
+        FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
+        fpp.addFilter(water);
+        GameStarter.getInstance().getViewPort().addProcessor(fpp);
+
+    }
     //kandarp
-    
 }
